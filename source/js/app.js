7056f32097b20ce2c97c70d39bbc4fb9c65c4e54
Joel <joel.ureellanah@gmail.com>
2018-06-21 09:30:58 +0100

    Made the page responsive and Scroll Reveal implemented

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,64 @@
 
-// JavaScript
-window.sr = ScrollReveal(); 
+function showNav() {
+  console.log("this is working");
+  var x = document.getElementById("responsive-nav");
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
+  if(x.className === "responsive-nav") {
+    x.className += " unfold";
+  } else {
+    x.className = "responsive-nav";
+  }
+}
+
+window.sr = ScrollReveal();
+sr.reveal('#header img.phone', {
+  duration: 300,
+  origin: 'bottom',
+  distance: '100px',
+});
+
+sr.reveal('#features div.car',{
+    delay: 200,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.shoe',{
+    delay: 400,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.device',{
+    delay: 600,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#discount .offer',{
+    delay: 600,
+    duration: 300,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('#discount picture',{
+    delay: 600,
+    duration: 300,
+    origin: 'left',
+    distance: '200px'
+});
+
+// sr.reveal('#download .apple', {
+//     delay: 500,
+//     duration: 300,
+//     origin: 'left',
+//     distance: '30px'
+// });
+//
+// sr.reveal('#download .google', {
+//     delay: 500,
+//     duration: 300,
+//     origin: 'right',
+//     distance: '30px'
 // });
